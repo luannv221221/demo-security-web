de634e0cd46a5452b946c584b6ba3276083a6467
your new username <smallmonster900@gmail.com>
2024-06-11 20:16:08 +0700

    init

---
+++ src/main/java/com/ra/model/entity/User.java
@@ -0,0 +1,29 @@
+package com.ra.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true,length = 100)
+    private String userName;
+    private String password;
+    private Boolean status;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns=@JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+}
